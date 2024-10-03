29bb03328cb33604e37d514b16430c17faba0bb4
isaacnanino <isaacnanino@gmail.com>
2024-10-03 05:58:29 -0400

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/AuthForm'
+
+function SignIn() {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
+  )
+}
+
+export default SignIn
